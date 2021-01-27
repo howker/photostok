d7a6cf3c98c741bdbdbcd3d0fb29e0d247552b38
howker <howker2007@gmail.com>
2021-01-27 12:00:40 +0400

    getTime added

--- lib/widgets/time_of_foto_creation.dart
+++ lib/widgets/time_of_foto_creation.dart
@@ -1,4 +1,3 @@
-//TODO time creation photo
 import 'package:flutter/material.dart';
 
 class TimeOfFotoCreation extends StatelessWidget {
@@ -6,8 +5,42 @@ class TimeOfFotoCreation extends StatelessWidget {
 
   const TimeOfFotoCreation({Key key, this.createdAt}) : super(key: key);
 
+  String getTime(String createData) {
+    String time = '';
+    Map monthAsString = {
+      1: 'january',
+      2: 'february',
+      3: 'march',
+      4: 'april',
+      5: 'may',
+      6: 'jun',
+      7: 'july',
+      8: 'august',
+      9: 'september',
+      10: 'october',
+      11: 'november',
+      12: 'december',
+    };
+    var timeDifference =
+        DateTime.now().difference(DateTime.parse(createData)).inHours;
+
+    if (timeDifference == 0) {
+      time = 'less then an hour ago';
+    }
+    if (timeDifference > 0 || timeDifference < 24) {
+      time = 'today';
+    }
+    if (timeDifference > 24) {
+      time =
+          '${DateTime.parse(createData).day} ${monthAsString[DateTime.parse(createData).month]} ${DateTime.parse(createData).year}';
+    }
+    print(time);
+    return time;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text(createdAt, style: Theme.of(context).textTheme.headline3);
+    return Text(getTime(createdAt),
+        style: Theme.of(context).textTheme.headline3);
   }
 }
