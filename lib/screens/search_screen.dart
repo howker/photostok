fbcedf0eae215d19c36520c835ccd2e264658b37
howker <howker2007@gmail.com>
2021-01-28 09:35:19 +0400

    buildSearchBar added

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -9,40 +9,45 @@ class SearchScreen extends StatelessWidget {
       child: Column(
         children: [
           const SizedBox(height: 34),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Container(
-              alignment: Alignment.center,
-              color: AppColors.gray,
-              width: double.infinity,
-              height: 36,
-              child: Row(
-                children: [
-                  const SizedBox(width: 12),
-                  const Icon(Icons.search, size: 20, color: AppColors.manatee),
-                  const SizedBox(width: 5),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration.collapsed(
-                        hintText: 'Search',
-                        hintStyle: Theme.of(context)
-                            .textTheme
-                            .headline2
-                            .copyWith(color: AppColors.manatee),
-                      ),
-                      cursorColor: AppColors.manatee,
-                      style: TextStyle(
-                        color: AppColors.manatee,
-                        fontSize: 23,
-                      ),
-                      onChanged: (inputText) {},
-                    ),
-                  ),
-                ],
+          _buildSearchBar(context),
+          const SizedBox(height: 10),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSearchBar(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        alignment: Alignment.center,
+        color: AppColors.gray,
+        width: double.infinity,
+        height: 36,
+        child: Row(
+          children: [
+            const SizedBox(width: 12),
+            const Icon(Icons.search, size: 20, color: AppColors.manatee),
+            const SizedBox(width: 5),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration.collapsed(
+                  hintText: 'Search',
+                  hintStyle: Theme.of(context)
+                      .textTheme
+                      .headline2
+                      .copyWith(color: AppColors.manatee),
+                ),
+                cursorColor: AppColors.manatee,
+                style: TextStyle(
+                  color: AppColors.manatee,
+                  fontSize: 23,
+                ),
+                onChanged: (inputText) {},
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
