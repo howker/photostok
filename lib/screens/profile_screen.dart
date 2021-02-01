53c7046007e9088edeb7b735491ec2caeea523b4
howker <howker2007@gmail.com>
2021-02-01 15:18:06 +0400

    ProfileScreen fix

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -7,11 +7,25 @@ import 'package:photostok/widgets/widgets.dart';
 class ProfileScreen extends StatelessWidget {
   final bool isMyProfile;
   final Photo photo;
-  const ProfileScreen({this.isMyProfile = true, this.photo});
+  const ProfileScreen({this.isMyProfile = false, this.photo});
 
   @override
   Widget build(BuildContext context) {
     if (isMyProfile)
+      return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: _buildAppBar(context),
+          body: TabBarView(
+            children: [
+              const Icon(Icons.directions_car),
+              const Icon(Icons.directions_transit),
+              const Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
+      );
+    else
       return DefaultTabController(
         length: 3,
         child: Scaffold(
@@ -23,56 +37,61 @@ class ProfileScreen extends StatelessWidget {
               'Profile',
               style: Theme.of(context).textTheme.headline2,
             ),
-            actions: [
-              _buildVerticalButton(context),
-            ],
-            bottom: TabBar(
-              labelColor: AppColors.blue,
-              unselectedLabelColor: AppColors.black,
-              indicatorWeight: 3,
-              tabs: [
-                Tab(icon: Icon(Icons.home)),
-                Tab(icon: Icon(Icons.favorite_border_outlined)),
-                Tab(icon: Icon(Icons.bookmark_border_outlined)),
-              ],
-            ),
           ),
           body: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: TabBarView(
+            child: Column(
               children: [
-                Icon(Icons.directions_car),
-                Icon(Icons.directions_transit),
-                Icon(Icons.directions_bike),
+                SizedBox(height: 200),
+                TabBar(
+                  labelColor: AppColors.blue,
+                  unselectedLabelColor: AppColors.black,
+                  indicatorWeight: 3,
+                  tabs: [
+                    Tab(icon: const Icon(Icons.home)),
+                    Tab(icon: const Icon(Icons.favorite_border_outlined)),
+                    Tab(icon: const Icon(Icons.bookmark_border_outlined)),
+                  ],
+                ),
+                Expanded(
+                  child: TabBarView(
+                    children: [
+                      const Icon(Icons.directions_car),
+                      const Icon(Icons.directions_transit),
+                      const Icon(Icons.directions_bike),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
         ),
       );
-    else
-      return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: AppColors.white,
-          centerTitle: true,
-          title: Text(
-            'Profile',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  UserAvatar(photo.user.profileImage.large),
-                ],
-              ),
-            ],
-          ),
-        ),
-      );
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: AppColors.white,
+      centerTitle: true,
+      title: Text(
+        'Profile',
+        style: Theme.of(context).textTheme.headline2,
+      ),
+      actions: [
+        _buildVerticalButton(context),
+      ],
+      bottom: TabBar(
+        labelColor: AppColors.blue,
+        unselectedLabelColor: AppColors.black,
+        indicatorWeight: 3,
+        tabs: [
+          Tab(icon: const Icon(Icons.home)),
+          Tab(icon: const Icon(Icons.favorite_border_outlined)),
+          Tab(icon: const Icon(Icons.bookmark_border_outlined)),
+        ],
+      ),
+    );
   }
 }
 
