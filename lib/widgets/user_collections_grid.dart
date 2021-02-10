50c40f18e5c0b47c434b2f58cceb43c88fa02d5c
howker <howker2007@gmail.com>
2021-02-10 16:58:59 +0400

    PhotoGridUserCollections fix

--- lib/widgets/user_collections_grid.dart
+++ lib/widgets/user_collections_grid.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:photostok/cubit/user_cubit.dart';
-import 'package:photostok/cubit/user_state.dart';
 import 'package:photostok/models/photo_list.dart';
+import 'package:photostok/models/user_collection.dart';
 import 'package:photostok/screens/detail_photo_screen.dart';
 import 'package:photostok/widgets/widgets.dart';
 
+///Сетка отображения коллекций пользователя
 class PhotoGridUserCollections extends StatelessWidget {
   final Photo photo;
   final String userName;
 
-  ///Сетка отображения коллекций пользователя
   const PhotoGridUserCollections({Key key, this.photo, this.userName})
       : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final _cubit = BlocProvider.of<UserCubit>(context);
-    _cubit.fetchUserCollections(1, 15, userName);
-    return BlocBuilder<UserCubit, UserState>(
-      builder: (context, state) {
-        if (state is UserCollectionsLoadFailure)
-          return ErrorLoadingBanner();
-        else if (state is UserCollectionsLoadSuccess) {
+    return FutureBuilder<UserCollectionList>(
+      builder: (context, snapshot) {
+        if (snapshot.hasData)
           return GridView.builder(
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisSpacing: 11, mainAxisSpacing: 9, crossAxisCount: 3),
@@ -43,24 +37,21 @@ class PhotoGridUserCollections extends StatelessWidget {
                   );
                 },
                 child: PhotoView(
-                  photoLink: state.userCollectionList.userCollectionList[index]
-                      .coverPhoto.urls.small,
-                  placeholderColor: state.userCollectionList
-                      .userCollectionList[index].coverPhoto.color,
+                  photoLink: snapshot
+                      .data.userCollectionList[index].coverPhoto.urls.small,
+                  placeholderColor:
+                      snapshot.data.userCollectionList[index].coverPhoto.color,
                   isRounded: true,
                   radiusPhoto: 7,
                 ),
               );
             },
-            itemCount: state.userCollectionList.userCollectionList.length,
+            itemCount: snapshot.data.userCollectionList.length,
           );
-        }
-        return Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            TripleCircularIndicator(),
-          ],
-        );
+        else
+          return Center(
+            child: Text('No content yet'),
+          );
       },
     );
   }
